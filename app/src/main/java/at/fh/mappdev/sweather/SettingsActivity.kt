f1599037387443c73014a33642312315f7305287
Adel Tuncer <adel.tuncer@edu.fh-joanneum.at>
2023-01-10 13:56:05 +0100

    Forgot to push Files

---
+++ app/src/main/java/at/fh/mappdev/sweather/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package at.fh.mappdev.sweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class SettingsActivity : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+       super.onCreate(savedInstanceState)
+   }
+
+
+}
