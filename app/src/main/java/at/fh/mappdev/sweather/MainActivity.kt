c6d2c543a57287bad3f9642b7e89507ea2b8bf21
Paul Strebenitzer <paul@strebenitzer.at>
2023-01-12 12:08:48 +0100

    fixed forecast layout design

--- app/src/main/java/at/fh/mappdev/sweather/MainActivity.kt
+++ app/src/main/java/at/fh/mappdev/sweather/MainActivity.kt
@@ -32,25 +32,6 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
         val sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
 
-        findViewById<Button>(R.id.logoutBtn).setOnClickListener() {
-            launch {
-                val session = sharedPreferences.getString(LoginFragment.SESSION, null)
-                if (session != null) {
-                    val result = apolloClient(applicationContext).query(LogoutQuery(session)).execute()
-                }
-
-                sharedPreferences.edit().remove(LoginFragment.SESSION).apply()
-                sharedPreferences.edit().remove(LoginFragment.ACCESS_TOKEN).apply()
-                sharedPreferences.edit().remove(LoginFragment.REFRESH_TOKEN).apply()
-                sharedPreferences.edit().remove(LoginFragment.EMAIL).apply()
-                sharedPreferences.edit().remove(LoginFragment.NAME).apply()
-
-                val intent = Intent(this@MainActivity, LoginRegisterActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }
-
         //locations button
         findViewById<Button>(R.id.locationBtn).setOnClickListener() {
             //toast message
