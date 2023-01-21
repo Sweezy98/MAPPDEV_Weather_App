46d9d30d8d167179f100eaf8f4434d6a9cae7b65
Paul Strebenitzer <paul@strebenitzer.at>
2023-01-21 16:12:55 +0100

    changed app share and rate actions

--- app/src/main/java/at/fh/mappdev/sweather/AboutActivity.kt
+++ app/src/main/java/at/fh/mappdev/sweather/AboutActivity.kt
@@ -3,6 +3,7 @@ package at.fh.mappdev.sweather
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.net.toUri
 import com.google.android.material.card.MaterialCardView
 
 class AboutActivity : AppCompatActivity() {
@@ -16,9 +17,19 @@ class AboutActivity : AppCompatActivity() {
                 action = Intent.ACTION_SEND
                 putExtra(
                     Intent.EXTRA_TEXT,
-                    "Hey, check out this cool app: https://play.google.com/store/apps/details?id=at.fh.mappdev.sweather"
+                    "Hey, check out this cool weather app: https://openweathermap.org/"
                 )
                 type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        // when clicking on the card Rate_App_Button, the project submission page is opened
+        findViewById<MaterialCardView>(R.id.Rate_App_Button).setOnClickListener() {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = "https://moodle-22-23.fh-joanneum.at/mod/assign/view.php?id=15204".toUri()
             }
             val shareIntent = Intent.createChooser(sendIntent, null)
             startActivity(shareIntent)
