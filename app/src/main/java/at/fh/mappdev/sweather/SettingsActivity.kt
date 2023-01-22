ca8f5cd51cc20d9461d16e40f57f3ec84ab17251
Adel Tuncer <adel.tuncer@edu.fh-joanneum.at>
2023-01-22 17:20:15 +0100

    Added Comments and Tests (not functional yet)

--- app/src/main/java/at/fh/mappdev/sweather/SettingsActivity.kt
+++ app/src/main/java/at/fh/mappdev/sweather/SettingsActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class SettingsActivity : AppCompatActivity(), CoroutineScope {
+    // Initializes Coroutine Scope
     private var job: Job = Job()
 
     override val coroutineContext: CoroutineContext
@@ -25,18 +26,17 @@ class SettingsActivity : AppCompatActivity(), CoroutineScope {
     }
 
     companion object {
-        const val UNIT = "UNIT"
-        const val IMAGE = "IMAGE"
+        const val UNIT = "UNIT" // Key for the unit
     }
 
    override fun onCreate(savedInstanceState: Bundle?) {
        super.onCreate(savedInstanceState)
-       val sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-       val name = sharedPreferences.getString(LoginFragment.NAME, null)
-       setContentView(R.layout.activity_settings)
-       findViewById<TextView>(R.id.locations).text = name
+       val sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE) // gets the shared preferences
+       val name = sharedPreferences.getString(LoginFragment.NAME, null) // gets the name from the shared preferences
+       setContentView(R.layout.activity_settings) // loads the Username from the shared preferences into
+       findViewById<TextView>(R.id.locations).text = name // sets the name of the user in the settings activity to the name from the shared preferences
 
-
+        // logs out the user
        findViewById<MaterialCardView>(R.id.logoutBtn_card).setOnClickListener() {
            launch {
                val session = sharedPreferences.getString(LoginFragment.SESSION, null)
@@ -45,29 +45,32 @@ class SettingsActivity : AppCompatActivity(), CoroutineScope {
                        apolloClient(applicationContext).query(LogoutQuery(session)).execute()
                }
 
-               sharedPreferences.edit().remove(LoginFragment.SESSION).apply()
-               sharedPreferences.edit().remove(LoginFragment.ACCESS_TOKEN).apply()
-               sharedPreferences.edit().remove(LoginFragment.REFRESH_TOKEN).apply()
-               sharedPreferences.edit().remove(LoginFragment.EMAIL).apply()
-               sharedPreferences.edit().remove(LoginFragment.NAME).apply()
+               sharedPreferences.edit().remove(LoginFragment.SESSION).apply() // removes the session from the shared preferences
+               sharedPreferences.edit().remove(LoginFragment.ACCESS_TOKEN).apply() // removes the access token from the shared preferences
+               sharedPreferences.edit().remove(LoginFragment.REFRESH_TOKEN).apply() // removes the refresh token from the shared preferences
+               sharedPreferences.edit().remove(LoginFragment.EMAIL).apply() // removes the email from the shared preferences
+               sharedPreferences.edit().remove(LoginFragment.NAME).apply() // removes the name from the shared preferences
+
 
                val intent = Intent(this@SettingsActivity, LoginRegisterActivity::class.java)
                startActivity(intent)
                finish()
            }
        }
-
+        // Button to change the User Password incase the User forgot theirs and got one sent to them by email
        findViewById<MaterialCardView>(R.id.ChangePWDBtn).setOnClickListener() {
            val intent = Intent(this@SettingsActivity, ResetPasswordActivity::class.java)
            startActivity(intent)
        }
-
+        // selects the celsius radio button
        findViewById<RadioButton>(R.id.Celsius).setOnClickListener() {
            sharedPreferences.edit().putString(UNIT, "Celsius").apply()
        }
+       // Selects the Fahrenheit Radio Button
        findViewById<RadioButton>(R.id.Fahrenheit).setOnClickListener() {
            sharedPreferences.edit().putString(UNIT, "Fahrenheit").apply()
        }
+       // Opens the AboutActivity when the about button is clicked
        findViewById<MaterialCardView>(R.id.AboutBtn_card).setOnClickListener() {
            val intent = Intent(this@SettingsActivity, AboutActivity::class.java)
            startActivity(intent)
@@ -75,8 +78,7 @@ class SettingsActivity : AppCompatActivity(), CoroutineScope {
 
 
    }
-
-    //check what unit is selected
+    // Keeps the selected unit in the settings, so that it doesnt reset when you go to another activity
     override fun onResume() {
         super.onResume()
         val sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
@@ -85,7 +87,6 @@ class SettingsActivity : AppCompatActivity(), CoroutineScope {
         if (unit == "Fahrenheit") {
             findViewById<RadioButton>(R.id.Fahrenheit).isChecked = true
         }
-
     }
 
 }
